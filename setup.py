400dab6b849f99cf2114cd6f849ff77fa010b446
Ahmad Murad Malik <ahmad@Ahmads-MacBook-Air.local>
2023-10-15 23:15:30 -0700

    Updated numpy requirements and incremented version number

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="ahmadsMathLibrary",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
-        "numpy==1.21.0",   # specify your dependencies here
+        "numpy>=1.21.0",   # specify your dependencies here
     ],
 )
